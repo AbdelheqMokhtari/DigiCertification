ae9888d1c80ab6d9616a7bb8085ba5662e100b2c
abdelheq <hakoumokhtari6@gmail.com>
2023-03-25 13:58:01 +0100

    ResNet 50

---
+++ Testing/ResNet 50.py
@@ -0,0 +1,27 @@
+from tensorflow.keras.applications.resnet50 import ResNet50
+from tensorflow.keras.layers import GlobalAveragePooling2D, Dense
+from tensorflow.keras.models import Model
+from tensorflow.keras.optimizers import Adam
+
+# Load the seed dataset and preprocess the data
+
+# Build the ResNet50 model
+base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+predictions = Dense(num_classes, activation='softmax')(x)
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# Compile the model
+model.compile(optimizer=Adam(lr=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+# Train the model
+model.fit(train_data, epochs=num_epochs, validation_data=val_data)
+
+# Evaluate the model
+model.evaluate(test_data)
+
+# Fine-tune the model
+
+# Predict using the model
+predictions = model.predict(new_data)
