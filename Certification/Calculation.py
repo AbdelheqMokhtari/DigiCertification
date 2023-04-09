fa2973131d13e0f6c672ea3d991b1cae6e6cec04
abdelheq <hakoumokhtari6@gmail.com>
2023-04-09 18:38:29 +0100

    CNCC Class

--- Certification/Calculation.py
+++ Certification/Calculation.py
@@ -9,19 +9,22 @@ class SeedCategory(Enum):
 
 
 class Seed:
-    def __init__(self, category):
+    def __init__(self, category, pure_seeds=None, inert_matter=None, other_species=None):
         if isinstance(category, SeedCategory):
             self.category = category
         else:
             raise ValueError("Invalid Category")
 
         # self.seed_category = seed_category
-        self.pure_seeds = None
-        self.total_seeds = None
+        self.pure_seeds = pure_seeds
+        self.total_seeds = 0
+        self.inert_matter = inert_matter
+        self.other_species = other_species
         # self.seed_category = SeedCategory.CATEGORY_1
 
     @property
     def purity_percentage(self):
+        self.total_seeds = self.pure_seeds + self.inert_matter + self.other_species
         return (self.pure_seeds / self.total_seeds) * 100
 
     @property
@@ -33,7 +36,7 @@ class Seed:
         if self.category == SeedCategory.CATEGORY_1:
             if purity >= 99:
                 self.category = SeedCategory.CATEGORY_1
-            elif purity >= 98:
+            elif 99 > purity >= 98:
                 self.category = SeedCategory.CATEGORY_2
             elif purity >= 97:
                 self.category = SeedCategory.CATEGORY_3
@@ -57,27 +60,39 @@ class Seed:
         else:
             self.category = SeedCategory.CATEGORY_4
 
+    @property
     def pure_seeds(self):
         return self.pure_seeds
 
     def pure_seeds(self, value):
         self.pure_seeds = value
 
-    def total_seeds(self):
-        return self.total_seeds
+    @property
+    def inert_matter(self):
+        return self.inert_matter
 
-    def total_seeds(self, value):
-        self.total_seeds = value
+    def inert_matter(self, value):
+        self.inert_matter = value
 
+    @property
+    def other_species(self):
+        return self.other_species
+
+    def other_species(self, value):
+        self.other_species = value
+
+    @property
     def seed_category(self):
-        return self.category
+        return self.category.value
 
 
-seed = Seed(SeedCategory.CATEGORY_2)
-seed.total_seeds = 200
-seed.pure_seeds = 195
-print(seed.seed_category().value)
+seed = Seed(SeedCategory.CATEGORY_1)
+seed.pure_seeds = float(input("semences pures (g) :"))
+seed.inert_matter = float(input("matiers inertes (g) :"))
+seed.other_species = float(input("Semance d'autre plants (g) :"))
+print(seed.seed_category)
 print(seed.purity_percentage)  # Output: 97.5
 seed.seed_category_update
 # print(seed.seed_category_update.value)  # Output: Ordinary
-print(seed.seed_category().value)
+print(seed.seed_category)
+
