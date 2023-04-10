6351c838125ee1ef260ffb71152ee391bf917031
abdelheq <hakoumokhtari6@gmail.com>
2023-04-10 15:13:44 +0100

    CNCC Class

--- Certification/Calculation.py
+++ Certification/Calculation.py
@@ -17,7 +17,6 @@ class Seed:
 
         # self.seed_category = seed_category
         self._pure_seeds = None
-        self._other_species = None
         self._grains_multees = None
         self._debris_vegeteux = None
         self._balles = None
@@ -25,6 +24,15 @@ class Seed:
         self._gravies = None
         self._other_im = None
         self._inert_matter = None
+        self._winter_wheat = None
+        self._winter_wheat_weight = None
+        self._barely = None
+        self._barely_weight = None
+        self._oats = None
+        self._oats_weight = None
+        self._triticale = None
+        self._triticale_weight = None
+        self._other_species = None
         self._total_seeds = None
         # self.seed_category = SeedCategory.CATEGORY_1
 
@@ -85,6 +93,70 @@ class Seed:
         self._other_im = value
 
     @property
+    def winter_wheat(self):
+        return self._winter_wheat
+
+    @winter_wheat.setter
+    def winter_wheat(self, value):
+        self._winter_wheat = value
+
+    @property
+    def winter_wheat_weight(self):
+        return self._winter_wheat_weight
+
+    @winter_wheat_weight.setter
+    def winter_wheat_weight(self, value):
+        self._winter_wheat_weight = value
+
+    @property
+    def barely(self):
+        return self._barely
+
+    @barely.setter
+    def barely(self, value):
+        self._barely = value
+
+    @property
+    def barely_weight(self):
+        return self._barely_weight
+
+    @barely_weight.setter
+    def barely_weight(self, value):
+        self._barely_weight = value
+
+    @property
+    def oats(self):
+        return self._oats
+
+    @oats.setter
+    def oats(self, value):
+        self._oats = value
+
+    @property
+    def oats_weight(self):
+        return self._oats_weight
+
+    @oats_weight.setter
+    def oats_weight(self, value):
+        self._oats_weight = value
+
+    @property
+    def triticale(self):
+        return self._triticale
+
+    @triticale.setter
+    def triticale(self, value):
+        self._triticale = value
+
+    @property
+    def triticale_weight(self):
+        return self._triticale_weight
+
+    @triticale_weight.setter
+    def triticale_weight(self, value):
+        self._triticale_weight = value
+
+    @property
     def other_species(self):
         return self._other_species
 
@@ -121,8 +193,21 @@ class Seed:
         self.inert_matter = self.sum_inert_mater
 
     @property
+    def sum_other_species(self):
+        addition = self.winter_wheat_weight + self.oats_weight + self.barely_weight + self.triticale_weight
+        return addition
+
+    @sum_other_species.setter
+    def sum_other_species(self, value):
+        pass  # do nothing when trying to set sum_attr1_attr2
+
+    def calculate_sum_and_set_other_species(self):
+        self.other_species = self.sum_other_species
+
+    @property
     def sum_total_seed(self):
         seed.calculate_sum_and_set_inert_matter()
+        seed.calculate_sum_and_set_other_species()
         addition = self.pure_seeds + self.other_species + self.inert_matter
         return addition
 
@@ -178,7 +263,7 @@ class Seed:
         return self.category.value
 
 
-seed = Seed(SeedCategory.CATEGORY_2)
+seed = Seed(SeedCategory.CATEGORY_1)
 seed.pure_seeds = float(input("semences pures (g) :"))
 print("--matiere inertes--")
 seed.grains_multees = float(input("Grains mutilées (g) :"))
@@ -187,7 +272,21 @@ seed.balles = float(input("balles (g) :"))
 seed.terres = float(input("Terres (g) :"))
 seed.gravies = float(input("Gravies (g) :"))
 seed.other_im = float(input("autres (g) :"))
-seed.other_species = float(input("Semance d'autre plants (g) :"))
+
+print("--other species--")
+
+seed.winter_wheat = int(input("winter wheat # :"))
+seed.winter_wheat_weight = float(input("winter wheat (g) :"))
+
+seed.barely = int(input("barely # :"))
+seed.barely_weight = float(input("barely (g) :"))
+
+seed.oats = float(input("oats # :"))
+seed.oats_weight = float(input("oats (g) :"))
+
+seed.triticale_weight = float(input("triticale # :"))
+seed.triticale_weight = float(input("triticale (g) :"))
+
 print(seed.seed_category)
 print(seed.purity_percentage)  # Output: 97.5
 seed.seed_category_update
