cf472ff7fbb52453c24a5a3cf2153c6e1ee187fd
abdelheq <hakoumokhtari6@gmail.com>
2023-05-11 04:35:16 +0100

    DensNet Model training

---
+++ Model_Construction/Deep Learning/DenseNet/Model/DesNet169.py
@@ -0,0 +1,67 @@
+import tensorflow as tf
+from keras.applications import DenseNet169
+from keras.layers import GlobalAveragePooling2D, Dense
+from keras.models import Model
+from keras.optimizers import Adam
+from keras.preprocessing.image import ImageDataGenerator
+
+physical_devices = tf.config.list_physical_devices('GPU')
+tf.config.experimental.set_memory_growth(physical_devices[0], True)
+
+train_datagen = ImageDataGenerator(rescale=1./255,
+                                   rotation_range=45)
+
+test_datagen = ImageDataGenerator(rescale=1./255)
+
+validation_datagen = ImageDataGenerator(rescale=1./255)
+
+train_data = train_datagen.flow_from_directory(
+    'Data/train',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+test_data = test_datagen.flow_from_directory(
+    'Data/test',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+validation_data = validation_datagen.flow_from_directory(
+    'Data/validation',
+    target_size=(224, 224),
+    batch_size=32,
+    class_mode='categorical'
+)
+
+num_classes = 8
+
+base_model = DenseNet169(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
+
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(256, activation='relu')(x)
+predictions = Dense(num_classes, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
+
+# freeze the base DenseNet121 Model
+for layer in base_model.layers:
+    layer.trainable = False
+
+model.compile(optimizer=Adam(lr=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
+
+model.fit(
+    train_data,
+    steps_per_epoch=train_data.n // train_data.batch_size,
+    epochs=20,
+    validation_data=validation_data,
+    validation_steps=validation_data.n // validation_data.batch_size
+)
+
+# Evaluate the model
+model.evaluate(test_data)
+
+# Save the model
+model.save('Model/DenseNet121epoch30.h5')
