e9eca94f745ff667c4187a4bceb474925831f754
abdelheq <hakoumokhtari6@gmail.com>
2023-06-12 02:03:33 +0100

    image segmentation

--- Image_Segmentation/image segmentation.py
+++ Image_Segmentation/image segmentation.py
@@ -1,5 +1,7 @@
 import cv2
 import os
+from skimage.filters import median
+from scipy.ndimage import gaussian_filter
 import numpy as np
 
 # Define Image Path
@@ -14,32 +16,30 @@ for folderName in folder:
         filepath = os.path.join(folderPath, filename)
         print("path", filepath)
         img = cv2.imread(filepath)
-        # Define the gamma value
-        gamma = 0.8
+        # Apply adaptive median filter
 
-        # Apply gamma correction
-        gamma_img = np.power(img / 255.0, gamma)
-        gamma_img = np.uint8(gamma_img * 255)
+        # Convert the image to grayscale
+        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        # Convert to grayscale
-        gray = cv2.cvtColor(gamma_img, cv2.COLOR_BGR2GRAY)
-        # Define the structuring element
-        kernel = np.ones((21, 21), np.uint8)
+        filtered_image_median = median(gray, behavior='adaptive', mode='reflect')
 
-        # Perform close operation
-        img_close = cv2.morphologyEx(gray, cv2.MORPH_CLOSE, kernel)
-
-        # Apply median blur
-        img_blur = cv2.medianBlur(img_close, 15)  # Here, 15 is the kernel size
+        # Apply adaptive Gaussian filter
+        filtered_image_guassian = gaussian_filter(filtered_image_median, sigma=3)
 
         # Apply Otsu's method to automatically determine the threshold value
-        thresh_val, thresh_img = cv2.threshold(img_blur, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+        thresh_val, thresh_img = cv2.threshold(filtered_image_guassian, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+
+        # Use the optimal threshold for Canny edge detection
+        edges = cv2.Canny(filtered_image_guassian, threshold1=0.5 * thresh_val, threshold2=thresh_val)
+
+        combined = cv2.bitwise_or(thresh_img, edges)
 
         # Find contours in the binary image
-        contours, hierarchy = cv2.findContours(thresh_img, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        contours, hierarchy = cv2.findContours(combined, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
         # Filter out small contours and draw bounding box around the remaining contours
         for contour in contours:
-            if cv2.contourArea(contour) > 1000:  # Minimum area threshold
+            if cv2.contourArea(contour) > 2000:  # Minimum area threshold
                 # Get the bounding box coordinates
                 x, y, w, h = cv2.boundingRect(contour)
                 print("Area =", cv2.contourArea(contour), "x =", x, "y =", y, "w =", w, "h =", h)
@@ -51,7 +51,7 @@ for folderName in folder:
                 height, width = crop_img.shape[:2]
 
                 # Set the desired output size
-                output_size = 350
+                output_size = 330
 
                 # Calculate the amount of padding needed on each side
                 h_pad = max(0, (output_size - height) // 2)
@@ -59,7 +59,7 @@ for folderName in folder:
 
                 # Add the padding using copyMakeBorder() function
                 output_img = cv2.copyMakeBorder(crop_img, h_pad, h_pad, w_pad, w_pad, cv2.BORDER_CONSTANT, value=(0, 0,
-                                                                                                                  0))
+                                                                                                                 0))
 
                 folder_name = f'Crop images/{folderName}'
                 # Create the folder and any necessary parent folders
