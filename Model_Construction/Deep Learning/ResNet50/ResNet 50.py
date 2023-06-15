3d1e15b6b96a2ded5a9dd5c1bc4ad0509e74d7f9
abdelheq <hakoumokhtari6@gmail.com>
2023-06-15 22:13:02 +0100

    ResNet 50 edit

--- Model_Construction/Deep Learning/ResNet50/ResNet 50.py
+++ Model_Construction/Deep Learning/ResNet50/ResNet 50.py
@@ -13,7 +13,9 @@ physical_devices = tf.config.list_physical_devices('GPU')
 tf.config.experimental.set_memory_growth(physical_devices[0], True)
 
 # List of class names
-class_names = ['Avoine', 'Ble dur', 'ble tendre', 'orge', 'triticale']
+# class_names = ['ble dur', 'Ble tendre', 'casee', 'echaudes', 'maigre', 'metadine', 'Mouchten', 'piqee']
+class_names = ['Bousselam', 'GTA', 'Oued el bared', 'Vitron']
+# class_names = ["ble dur", "Ble tendre", "casee", "echaudes", "maigre", "Metadine", "Mouchten", "piqee"]
 
 
 def save_history_json(history, file_path):
@@ -31,7 +33,8 @@ checkpoint_callback = tf.keras.callbacks.ModelCheckpoint(
     mode='min'  # or 'max' depending on the metric being monitored
 )
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255
+                                   )
 
 test_datagen = ImageDataGenerator(rescale=1./255)
 
@@ -58,25 +61,34 @@ validation_data = validation_datagen.flow_from_directory(
 )
 
 # Build the ResNet50 model
-num_classes = 5
-num_epochs = 50
+num_classes = 4
+num_epochs = 75
 base_model = ResNet50(weights='imagenet', include_top=False, input_shape=(224, 224, 3))
-# x = base_model.output
-# x = GlobalAveragePooling2D()(x)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = base_model.output
+x = GlobalAveragePooling2D()(x)
+x = Dense(512, activation='relu')(x)
+x = Dense(256, activation='relu')(x)
+x = Dense(128, activation='relu')(x)
+x = Dense(64, activation='relu')(x)
+predictions = Dense(num_classes, activation='softmax')(x)
+
+model = Model(inputs=base_model.input, outputs=predictions)
 # predictions = Dense(num_classes, activation='softmax')(x)
 
 # Create a new model by adding custom classification layers
-model = Sequential()
-model.add(base_model)
-model.add(Flatten())
-model.add(Dense(512, activation='relu'))
-model.add(Dense(256, activation='relu'))
-model.add(Dense(128, activation='relu'))
-model.add(Dense(num_classes, activation='softmax'))
+# model = Sequential()
+# model.add(base_model)
+# model.add(Flatten())
+# model.add(Dense(512, activation='relu'))
+# model.add(Dense(256, activation='relu'))
+# model.add(Dense(128, activation='relu'))
+# model.add(Dense(num_classes, activation='softmax'))
 
 # model = Model(inputs=base_model.input, outputs=predictions)
 # Compile the model
-model.compile(optimizer=Adam(learning_rate=0.0001), loss='categorical_crossentropy', metrics=['accuracy'])
+model.compile(optimizer=Adam(learning_rate=0.0005), loss='categorical_crossentropy', metrics=['accuracy'])
 
 # Say not to train first layer (ResNet) model as it is already trained
 # model.layers[0].trainable = True
@@ -90,37 +102,65 @@ num_layers_to_unfreeze = 10
 for layer in model.layers[-num_layers_to_unfreeze:]:
     layer.trainable = True
 
-# Train the model
+# Train.txt the model
 history = model.fit(train_data, epochs=num_epochs, validation_data=validation_data, verbose=1,
                     callbacks=[checkpoint_callback])
 
-save_history_json(history, 'history/CNCC/Resnet50_epochs50_history_false_unfreeze10.json')
+# Load the best weights after training
+model.load_weights('callbacks/best_weights.h5')
+
+save_history_json(history, 'history/varities/ResNet50/ResNet50_epochs75_unfreeze10_history_best_weight.json')
 
 # Evaluate the model
 model.evaluate(test_data)
 
 # Save the model
-model.save('Model/CNCC/ResNet50_epochs50_false_unfreeze10.h5')
+model.save('Model/varities/ResNet50/ResNet50_epochs75_unfreeze10_model_best_weight.h5')
 
 # Save class names as attributes of the HDF5 file
-with h5py.File('Model/CNCC/ResNet50_epochs50_false_unfreeze10.h5', 'a') as file:
+with h5py.File('Model/varities/ResNet50/ResNet50_epochs75_unfreeze10_model_best_weight.h5', 'a') as file:
     file.attrs['class_names'] = class_names
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # Create a graph of the training accuracy with respect to epoch values using a library like Matplotlib
 
-train_accuracy = history.history['accuracy']
-test_accuracy = history.history['val_accuracy']
-epoch_values = np.arange(1, num_epochs+1)
+# train_accuracy = history.history['accuracy']
+# test_accuracy = history.history['val_accuracy']
+# epoch_values = np.arange(1, num_epochs+1)
 # create a plot of training accuracy vs epoch values
-fig, ax = plt.subplots()
-ax.plot(epoch_values, train_accuracy, label='Training Accuracy')
-ax.plot(epoch_values, test_accuracy, label='Validation Accuracy')
-ax.set_xticks(np.arange(0, num_epochs+1, step=1))
-ax.set_xlabel('Epoch')
-ax.set_ylabel('Accuracy')
-ax.set_title('Training and Validation Accuracy vs Epoch')
-ax.legend(loc='lower right')
-plt.show()
+# fig, ax = plt.subplots()
+# ax.plot(epoch_values, train_accuracy, label='Training Accuracy')
+# ax.plot(epoch_values, test_accuracy, label='Validation Accuracy')
+# ax.set_xticks(np.arange(0, num_epochs+1, step=1))
+# ax.set_xlabel('Epoch')
+# ax.set_ylabel('Accuracy')
+# ax.set_title('Training and Validation Accuracy vs Epoch')
+# ax.legend(loc='lower right')
+# plt.show()
 
 # Save the plot as a PNG image
 # fig.savefig('accuracy_vs_epoch20.png', dpi=300, bbox_inches='tight')
